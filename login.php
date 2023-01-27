613fed91cac5086ee1f36afc66c463c2d93cea22
Geriprof <szurigergo@gmail.com>
2023-01-27 17:13:25 +0100

    init

--- login.php
+++ login.php
@@ -38,7 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
+    <title>Documentt</title>
 </head>
 
 <body class="flex justify-center items-center h-screen w-screen">
